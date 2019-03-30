c53d2e215fa6535c32a9aa1f2254e2d79882ca5c
Kshitij Yadav <kshitij.yadav@threadsol.com>
2019-03-30 17:32:27 +0530

    shared_preferences_saving_value_in_phone

--- app/src/main/java/com/example/user/myudemyapp/MainActivity.kt
+++ app/src/main/java/com/example/user/myudemyapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.user.myudemyapp
 
+import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
@@ -18,5 +19,18 @@ class MainActivity : AppCompatActivity() {
             i.putExtra("name", editText2.text.toString())
             startActivity(i)
         }
+
+        btn_write.setOnClickListener {
+            var sp = getSharedPreferences("my_data", Context.MODE_PRIVATE)
+            var editor = sp.edit()
+            editor.putString("name", editText2.text.toString())
+            editor.commit()
+        }
+
+        btn_read.setOnClickListener {
+            var sp = getSharedPreferences("my_data", Context.MODE_PRIVATE)
+            Toast.makeText(this, "SP name is " + sp.getString("name", ""), Toast.LENGTH_LONG).show()
+        }
     }
+
 }
