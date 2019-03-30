0772d6c75355cb3ee9cc327594d1615a2304b331
Kshitij Yadav <kshitij.yadav@threadsol.com>
2019-03-30 19:48:13 +0530

    picasso_image_from_server_implemnetation

--- app/src/main/java/com/example/user/myudemyapp/MainActivity.kt
+++ app/src/main/java/com/example/user/myudemyapp/MainActivity.kt
@@ -13,6 +13,7 @@ import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -21,64 +22,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button2.setOnClickListener {
-            var i=Intent(this, SecondAct::class.java)
-            i.putExtra("name", editText2.text.toString())
-            startActivity(i)
-        }
-
-        btn_write.setOnClickListener {
-            var sp = getSharedPreferences("my_data", Context.MODE_PRIVATE)
-            var editor = sp.edit()
-            editor.putString("name", editText2.text.toString())
-            editor.commit()
-        }
-
-        btn_read.setOnClickListener {
-            var sp = getSharedPreferences("my_data", Context.MODE_PRIVATE)
-            Toast.makeText(this, "SP name is " + sp.getString("name", ""), Toast.LENGTH_LONG).show()
-        }
-
-        btn_volley.setOnClickListener {
-            var url:String = "http://192.168.43.19:3001/test"
-            var rq:RequestQueue = Volley.newRequestQueue(this)
-            var sr = StringRequest(Request.Method.GET, url, Response.Listener { response ->
-                var msg:String = "Success: " + response
-                Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
-            }, Response.ErrorListener {error ->
-                Toast.makeText(this, error.message, Toast.LENGTH_LONG).show()
-            })
-
-            rq.add(sr)
-        }
-
-        btn_volley_json.setOnClickListener {
-            var url:String = "http://192.168.43.19:3001/test?type=json"
-            var rq:RequestQueue = Volley.newRequestQueue(this)
-            var jor = JsonObjectRequest(Request.Method.GET, url, null, Response.Listener { response ->
-                var msg:String = "Success: Name=" + response.getString("name") + " salary=" + response.getString("salary")
-                Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
-            }, Response.ErrorListener {error ->
-                Toast.makeText(this, error.message, Toast.LENGTH_LONG).show()
-            })
-
-            rq.add(jor)
-        }
-
-        btn_volley_array.setOnClickListener {
-            var url:String = "http://192.168.43.19:3001/test?type=array"
-            var rq:RequestQueue = Volley.newRequestQueue(this)
-            var jar = JsonArrayRequest(Request.Method.GET, url, null, Response.Listener { response ->
-                var msg:String = "Success: "
-                for(i in 0..response.length()-1)
-                    msg += "Name=" + response.getJSONObject(i).getString("name") + " salary=" + response.getJSONObject(i).getString("salary")
-                Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
-            }, Response.ErrorListener {error ->
-                Toast.makeText(this, error.message, Toast.LENGTH_LONG).show()
-            })
-
-            rq.add(jar)
-        }
+        var url:String = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+        Picasso.get().load(url).into(imageView2)
     }
 
 }
